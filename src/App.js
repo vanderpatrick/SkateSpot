9655054fe03aee9f77575cea2bd52ef91c93ab79
vanderpatrick <patrick123321123@icloud.com>
2022-11-24 12:49:10 +0000

    Add signup and signin form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Container className={styles.Main} >
         <Switch>
         <Route exact path="/" render={() => <h1>Home page</h1>} />
-        <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+        <Route exact path="/signin" render={() => <SignIn /> } />
         <Route exact path="/signup" render={() => <SignUp />} />
         <Route render={() => <p>Page not found!</p>} />
         </Switch>
