4244eaf5124d46ecb758cd830fe63e963f73a38e
vanderpatrick <patrick123321123@icloud.com>
2022-11-24 13:20:38 +0000

    Add axios and event handlers to singup form

--- src/Pages/registrations/SignUp.jsx
+++ src/Pages/registrations/SignUp.jsx
@@ -1,25 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import styles from "../../styles/Signup.module.css"
+import styles from "../../styles/Signup.module.css";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 const SignUp = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const handleSubmit = async(event) => {
+    event.preventDefault()
+    try{
+      await axios.post("/dj-rest-auth/registration/", signUpData)
+      history.push('/signin')
+    }catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <Form className={styles.FormContainer}>
-      <h1 className="text-center mb-5">Welcome to skateSpot please fill in your info</h1>
+    <Form onSubmit={handleSubmit} className={styles.FormContainer}>
+      <h1 className="text-center mb-5">
+        Welcome to skateSpot please fill in your info
+      </h1>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" />
+        <Form.Control
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+          placeholder="Enter username"
+        />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
+      <Form.Group className="mb-3">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter password" />
+        <Form.Control
+          type="password"
+          name="password1"
+          onChange={handleChange}
+          value={password1}
+          placeholder="Enter password"
+        />
       </Form.Group>
-      <Form.Group className="mb-3" >
+      <Form.Group className="mb-3">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Confirm password" />
+        <Form.Control
+          type="password"
+          name="password2"
+          onChange={handleChange}
+          value={password2}
+          placeholder="Confirm password"
+        />
       </Form.Group>
-      
+
       <Button className={styles.FormButton} type="submit">
         Submit
       </Button>
---
+++ src/api/Axios.jsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://skt-drf.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
