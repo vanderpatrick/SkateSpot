8632d91912ad2cb38bf286d06c40b6362dcc54d7
vanderpatrick <patrick123321123@icloud.com>
2022-11-27 15:16:21 +0000

    Add file input and post form content

--- src/Pages/creations/CreatePost.jsx
+++ src/Pages/creations/CreatePost.jsx
@@ -1,49 +1,86 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import styles from "../../styles/CreatePost.module.css";
-import { Row, Form, Col, Figure } from "react-bootstrap";
-const CreatePost = () => {
+import { Row, Form, Col, Figure, Image } from "react-bootstrap";
+function PostCreateForm() {
   const [postData, setPostData] = useState({
     title: "",
     content: "",
     image: "",
   });
   const { title, content, image } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
   return (
     <Form className={styles.FormContainer}>
       <Row>
         <Col className="d-flex justify-content-center align-items-center">
           <Form.Group>
-            <Form.Label htmlFor="image-upload">
-              <Figure className={styles.UploadSize}>
-                <i class="fa-solid fa-upload"></i>
-              </Figure>
-            </Form.Label>
-            
+            {image ? (
+              <>
+                <Figure>
+                  <Image className={styles.Image} src={image} rounded />
+                </Figure>
+              </>
+            ) : (
+              <Form.Label htmlFor="image-upload">
+                <Figure className={styles.UploadSize}>
+                  <i className="fa-solid fa-upload"></i>
+                </Figure>
+              </Form.Label>
+            )}
+            <Form.Control
+              type="file"
+              className="d-none"
+              id="image-upload"
+              accept="image/*"
+              onChange={handleChangeImage}
+            />
           </Form.Group>
         </Col>
         <Col>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Title :</Form.Label>
-            <Form.Control type="text" name="title" value={title} />
+            <Form.Control
+              type="text"
+              name="title"
+              onChange={handleChange}
+              value={title}
+            />
           </Form.Group>
 
-          <Form.Group className="mb-3">
+          <Form.Group className="mb-5">
             <Form.Label>Content :</Form.Label>
             <Form.Control
               as="textarea"
               rows={5}
               name="content"
               value={content}
+              onChange={handleChange}
             />
           </Form.Group>
         </Col>
       </Row>
-      <Button className={styles.FormButton} type="submit">
+      <Button  className={styles.FormButton} type="submit">
         Submit
       </Button>
     </Form>
   );
-};
+}
 
-export default CreatePost;
+export default PostCreateForm;
