367be7fa06b6652532897e125e20a6a95f48f8ba
vanderpatrick <patrick123321123@icloud.com>
2022-11-28 13:32:21 +0000

    Add like and comment buttons to post component

--- src/Pages/posts/Post.jsx
+++ src/Pages/posts/Post.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
 import Avatart from "../../components/Avatart";
 import styles from "../../styles/Post.module.css";
 import { useCurrentuser } from "../../contexts/CurrentUserContext";
@@ -11,7 +11,7 @@ const Post = (props) => {
     profile_id,
     profile_image,
     comments_count,
-    likes_count,
+    like_count,
     like_id,
     title,
     content,
@@ -35,10 +35,39 @@ const Post = (props) => {
         {updated_at}
       </Card.Header>
       <Card.Img variant="top" src={image} />
-      <Card.Body className={styles.Card}>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{content}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+      <Card.Body>
+        {title && <Card.Title>{title} </Card.Title>}
+        {content && <Card.Text className="text-center">{content}</Card.Text>}
+        <div className={styles.Space}>
+          {is_owner ? (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own post!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          ) : like_id ? (
+            <span onClick={() => {}}>
+              <i className={`fas fa-heart ${styles.Heart}`} />
+            </span>
+          ) : currentUser ? (
+            <span onClick={() => {}}>
+              <i className={`far fa-heart ${styles.HeartOutline}`} />
+            </span>
+          ) : (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Log in to like posts!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          )}
+          {like_count}
+          <Link to={`/posts/${id}`}>
+            <i className="far fa-comments" />
+          </Link>
+          {comments_count}
+        </div>
       </Card.Body>
     </Card>
   );
